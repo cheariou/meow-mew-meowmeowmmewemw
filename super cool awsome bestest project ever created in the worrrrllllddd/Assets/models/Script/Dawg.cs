18828cc6146d3654aec61bc4883fe6937607763e
cheariou <t4st3bud2@gmail.com>
2024-09-11 09:53:23 -0700

    heheheh
    lalallala

---
+++ super cool awsome bestest project ever created in the worrrrllllddd/Assets/models/Script/Dawg.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public Transform targetObj;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(this.transform.position, TargetObj.Position, 10 * Time.deltaTime);
+    }
+}
