62b28f937528a15017da31ddaf6e578f671f8934
cheariou <t4st3bud2@gmail.com>
2024-09-17 09:47:40 -0700

    Update PlayerControler.cs

--- super cool awsome bestest project ever created in the worrrrllllddd/Assets/models/Script/PlayerControler.cs
+++ super cool awsome bestest project ever created in the worrrrllllddd/Assets/models/Script/PlayerControler.cs
@@ -4,18 +4,23 @@ using UnityEngine;
 
 public class PlayerControler : MonoBehaviour
 {
-    Rigidbody myRB ;
+    Rigidbody myRB;
     Camera playerCam;
 
     Vector2 camRotation;
 
+    [Header("Movement Stats")]
+    public bool sprinting = false;
+    public bool sprintToggle = false;
     public float speed = 10f;
+    public float sprintMult = 1.5f;
     public float jumpHeight = 5f;
-
+    public float groundDection = 1f; 
     public float mouseSensitivity = 2.0f;
     public float xsensitivity = 2.0f;
     public float ysensitivity = 2.0f;
 
+    [Header("User Settings")]
     public float camrotationLimit = 90f;
 
     // Start is called before the first frame update
@@ -40,12 +45,27 @@ public class PlayerControler : MonoBehaviour
         playerCam.transform.localRotation = Quaternion.AngleAxis(camRotation.y, Vector3.left);
         transform.localRotation = Quaternion.AngleAxis(camRotation.x, Vector3.up);
 
+        if(!sprinting && !sprintToggle && Input.GetKey(KeyCode.LeftShift))
+            sprinting= true;
+
+        if (!sprinting && sprintToggle && (Input.GetAxisRaw("vertical") > 0) && Input.GetKey(KeyCode.LeftShift))
+            sprinting = true;
+
         Vector3 temp = myRB.velocity;
 
             temp.x = Input.GetAxisRaw("Horizontal") * speed;
             temp.z = Input.GetAxisRaw("Vertical") * speed;
 
-        if(Input.GetKeyDown(KeyCode.Space))
+        if (sprinting)
+            temp.z *= sprintMult;
+
+        if (sprinting && Input.GetKeyUp(KeyCode.LeftShift))
+            sprinting= false;
+
+        if (sprinting && sprintToggle && (Input.GetAxisRaw("vertical") <= 0))
+            sprinting = false;
+
+        if (Input.GetKeyDown(KeyCode.Space) && Physics.Raycast(transform.position, -transform.up, groundDection))
             temp.y = jumpHeight;
 
         myRB.velocity = (transform.forward * temp.z) + (transform.right * temp.x) + (transform.up * temp.y);
